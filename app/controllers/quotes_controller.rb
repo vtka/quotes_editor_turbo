ed802f8b0424056baf63a190aaa119255e50225c
vtka <slavik.tkachenko@gmail.com>
2022-08-24 14:59:52 +0600

    Turbo flash

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -13,13 +13,12 @@ class QuotesController < ApplicationController
   end
 
   def create
-    # Only this first line changes to make sure the association is created
     @quote = current_company.quotes.build(quote_params)
   
     if @quote.save
       respond_to do |format|
         format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
-        format.turbo_stream
+        format.turbo_stream { flash.now[:notice] = "Quote was successfully created." }
       end
     else
       render :new
@@ -31,7 +30,10 @@ class QuotesController < ApplicationController
 
   def update
     if @quote.update(quote_params)
-      redirect_to quotes_path, notice: "Quote was successfully updated."
+      respond_to do |format|
+        format.html { redirect_to quotes_path, notice: "Quote was successfully updated." }
+        format.turbo_stream { flash.now[:notice] = "Quote was successfully updated." }
+      end
     else
       render :edit, status: :unprocessable_entity
     end
@@ -41,8 +43,8 @@ class QuotesController < ApplicationController
     @quote.destroy
   
     respond_to do |format|
-      format.html { redirect_to quotes_path, notice: "Quote was successfully destroyed." }
-      format.turbo_stream
+      format.html { redirect_to quotes_path, notice: "" }
+      format.turbo_stream { flash.now[:notice] = "Quote was successfully destroyed." }
     end
   end
 
