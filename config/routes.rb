61fdcfe1e443b4281164c97604cffa303595d891
vtka <slavik.tkachenko@gmail.com>
2022-08-24 15:55:14 +0600

    LineItemDates

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "pages#home"
-  resources :quotes
+  
+  resources :quotes do
+    resources :line_item_dates, except: [:index, :show]
+  end
 end
