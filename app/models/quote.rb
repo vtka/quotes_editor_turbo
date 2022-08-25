bcea5b81dee45c398f47672313de1cd76c2457d8
vtka <slavik.tkachenko@gmail.com>
2022-08-25 14:07:56 +0600

    Turbo total and specs

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,6 @@
 class Quote < ApplicationRecord
   has_many :line_item_dates, dependent: :destroy
+  has_many :line_items, through: :line_item_dates
   belongs_to :company
   
   validates :name, presence: true
@@ -7,4 +8,8 @@ class Quote < ApplicationRecord
   scope :ordered, -> { order(id: :desc) }
 
   broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
+
+  def total_price
+    line_items.sum(&:total_price)
+  end
 end
